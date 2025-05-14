63455751292ae5d520609145abf6e4ccfe2dc328
a-kusumoto-quolim <a-kusumoto@hibarijyuku.com>
2025-05-14 16:12:03 +0900

    [フッター]修正

--- src/js/footer-component.js
+++ src/js/footer-component.js
@@ -1,25 +1,28 @@
 export default {
   template: `<div class="w-full mx-auto flex flex-wrap flex-row bg-[rgb(102,102,102)] text-white">
-  <div class="basis-full flex items-center md:px-10 pl-5 py-5">
-    <div class="flex md:flex-row flex-col w-full py-5">
+  <div class="basis-full flex items-center md:px-10 p-5">
+    <div class="flex md:flex-row flex-col w-full">
       <div class="flex flex-col md:flex-1 items-center">
         <img src="/img/logo.png" alt="株式会社アイワ商事 ロゴ" class="h-36 w-auto object-contain" />
         <div class="font-extrabold text-[5vw] whitespace-nowrap">
-          <div class="md:text-xl">断熱資材・産業資材の総合商社</div>
-          <div class="md:text-3xl">株式会社 アイワ商事</div>
-          <div class="md:text-sm">Aiwasyoji Trading Company.co.,Ltd</div>
+          <p class="md:text-xl">断熱資材・産業資材の総合商社</p>
+          <p class="md:text-3xl">株式会社 アイワ商事</p>
+          <p class="md:text-sm">Aiwasyoji Trading Company.co.,Ltd</p>
         </div>
       </div>
-      <div class="flex flex-col justify-center md:flex-1 items-center py-5">
-        <div class="font-extrabold text-[5vw] whitespace-nowrap">
-          <div class="md:text-lg">アクセス</div>
-          <div class="md:text-lg">〒857-0852 長崎県佐世保市干尽町 3-7</div>
-          <div class="md:text-lg">TEL 0956-33-6301</div>
-          <div class="md:text-lg">FAX 0956-33-7312</div>
+      <div class="flex flex-col justify-center md:flex-1 p-5">
+        <div class="font-extrabold text-[5vw]">
+          <p class="md:text-lg">アクセス</p>
+          <p class="md:text-lg">
+            <span class="block lg:inline">〒857-0852</span>
+            <span class="block lg:inline">長崎県佐世保市干尽町3-7</span>
+          </p>
+          <p class="md:text-lg">TEL 0956-33-6301</p>
+          <p class="md:text-lg">FAX 0956-33-7312</p>
         </div>
       </div>
       <div class="items-center md:flex-1">
-        <iframe class="md:w-[30vw] w-[90vw] mt-6 aspect-video" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3340.388525958434!2d129.7247714755211!3d33.15142546475831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356a95d091c20f39%3A0x2ca8b061a4f25f17!2zKOagqinjgqLjgqTjg6_llYbkuos!5e0!3m2!1sja!2sjp!4v1746947020129!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="md:w-[30vw] w-[90vw] mt-6 max-w-full aspect-video" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3340.388525958434!2d129.7247714755211!3d33.15142546475831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356a95d091c20f39%3A0x2ca8b061a4f25f17!2zKOagqinjgqLjgqTjg6_llYbkuos!5e0!3m2!1sja!2sjp!4v1746947020129!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>  
   </div>
